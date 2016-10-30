a2e1d38c0ac5f7dbd0d67801b0efdb15923f103b
Michaël Dierick <michael@dierick.io>
2016-10-30 15:34:07 +0100

    Added parsing function for MALLET trainingset keys-file + refactoring

--- mallet_tools.py
+++ mallet_tools.py
@@ -1,19 +1,26 @@
-#!/usr/bin/python3
 """
-Generic functions for processing MALLET output files
+Generic functions for parsing MALLET files
 """
-
-def process_line(l):
-    parts = l.split('\t')
-    nr = int(parts[0])
-    path = parts[1]
-    weights = {}
-    for i in range(2,len(parts),2):
-        key, val = parts[i:i+2]
-        weights[int(key)] = float(val)
-    return (nr, path, weights)
 
 def process_file(path):
+    """parse a mallet inferencer output file"""
+    def parse_line(l):
+        parts = l.split('\t')
+        nr = int(parts[0])
+        path = parts[1].lstrip('file:')
+        weights = {}
+        for i in range(2, len(parts), 2):
+            key, val = parts[i:i+2]
+            weights[int(key)] = float(val)
+        return (nr, path, weights)
     with open(path, 'r') as f:
         next(f)#skip first line
-        return [process_line(l.rstrip('\n')) for l in f]
+        return [parse_line(l.rstrip('\n')) for l in f]
+
+def parse_topicfile(path):
+    """parse a mallet trainingset keys file"""
+    def parse_line(l):
+        parts = l.split('\t')
+        return (int(parts[0]), float(parts[1]), list(parts[2:]))
+    with open(path, 'r') as f:
+        return [parse_line(l.rstrip('\n')) for l in f]
