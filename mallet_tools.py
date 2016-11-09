80786ee3d35974d88fa5eaecf0db64925b55c34c
Michaël Dierick <michael@dierick.io>
2016-11-09 15:08:19 +0100

    Improved function documentation

--- mallet_tools.py
+++ mallet_tools.py
@@ -3,7 +3,11 @@ Generic functions for parsing MALLET files
 """
 
 def process_file(path):
-    """parse a mallet inferencer output file"""
+    """
+    parse a MALLET inferencer output file
+    returns a list of tuples with structure:
+    (id, filepath, {5: 0,362865, ...})
+    """
     def parse_line(l):
         parts = l.split('\t')
         nr = int(parts[0])
@@ -18,7 +22,11 @@ def process_file(path):
         return [parse_line(l.rstrip('\n')) for l in f]
 
 def parse_topicfile(path):
-    """parse a mallet trainingset keys file"""
+    """
+    parse a MALLET trainingset "keys.txt"-file
+    returns a list of tuples with structure:
+    (id, ?, ["water", "wind", "ocean", ...])
+    """
     def parse_line(l):
         parts = l.split('\t')
         return (int(parts[0]), float(parts[1]), list(parts[2:]))
